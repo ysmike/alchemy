3f3d662939b7980aa39405ea8f44661cc7b2f8bc
ysmike <xcalibur220@gmail.com>
2021-11-19 12:34:33 -0500

    apply styles on the server

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,6 +1,18 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import Document, { Html, Head, NextScript, Main } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+  // get the page to render, extract the styles, and return both
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
   render() {
     return (
       <Html lang="en-US">
