88ff147db429c2527a0f6645e156833ad5634b72
ysmike <xcalibur220@gmail.com>
2021-11-22 17:26:12 -0500

    fix gray to lightgray

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -14,8 +14,8 @@ const GlobalStyles = createGlobalStyle`
     --black: #393939;
     --grey: #3a3a3a;
     --gray: var(--grey);
-    --lightGray: #e1e1e1;
-    --lightGrey: var(--grey);
+    --lightGrey: #e1e1e1;
+    --lightGray: var(--lightGrey);
     --offWhite: #ededed;
     --maxWidth: 1000px;
     --bs: 0 12px 24px 0 rgba(0,0,0,0.09);
