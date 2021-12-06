b4d51ff9752815bd73d0ff4d60c9b4081b6409c7
ysmike <xcalibur220@gmail.com>
2021-12-06 18:37:59 -0500

    implement programmatically inserting seed data

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -8,6 +8,7 @@ import {
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-alchemy';
@@ -38,7 +39,11 @@ export default withAuth(
     db: {
       adapter: 'mongoose',
       url: databaseURL,
-      // TODO: Add data seeding here
+      async onConnect(keystone) {
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
     },
     lists: createSchema({
       // Schema items go in here
