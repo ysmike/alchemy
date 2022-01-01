ef1a6f7c06714698bc6687613b179cef7efd41e4
ysmike <xcalibur220@gmail.com>
2021-12-31 23:10:04 -0500

    initialize cart state using React Context

---
+++ frontend/lib/cartState.js
@@ -0,0 +1,37 @@
+import { createContext, useContext, useState } from 'react';
+import Cart from '../components/Cart';
+
+const LocalStateContext = createContext();
+const LocalStateProvider = LocalStateContext.Provider;
+
+function CartStateProvider({ children }) {
+  // this is our own custom provider. we'll store data (state) and functionality
+  // (updates) in here and anyone can access via the consumer
+
+  const [cartOpen, setCartOpen] = useState(false);
+  function toggleCart() {
+    setCartOpen(!cartOpen);
+  }
+  function closeCart() {
+    setCartOpen(false);
+  }
+  function openCart() {
+    setCartOpen(true);
+  }
+  return (
+    <LocalStateProvider
+      value={{ cartOpen, setCartOpen, toggleCart, closeCart, openCart }}
+    >
+      {children}
+    </LocalStateProvider>
+  );
+}
+
+// make a custom hook for accessing the cart local state
+function useCart() {
+  // use a consumer to access the local state
+  const all = useContext(LocalStateContext);
+  return all;
+}
+
+export { CartStateProvider, useCart };
