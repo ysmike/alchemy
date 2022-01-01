223cd51d01a53a74f8b990c84c47afb2305d27c8
ysmike <xcalibur220@gmail.com>
2021-12-31 23:15:00 -0500

    wrap React Context Provider into the app

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -4,6 +4,7 @@ import { ApolloProvider } from '@apollo/client';
 import Page from '../components/Page';
 import 'nprogress/nprogress.css';
 import withData from '../lib/withData';
+import { CartStateProvider } from '../lib/cartState';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
@@ -13,9 +14,11 @@ Router.events.on('routeChangeError', () => NProgress.done());
 function MyApp({ Component, pageProps, apollo }) {
   return (
     <ApolloProvider client={apollo}>
-      <Page>
-        <Component {...pageProps} />
-      </Page>
+      <CartStateProvider>
+        <Page>
+          <Component {...pageProps} />
+        </Page>
+      </CartStateProvider>
     </ApolloProvider>
   );
 }
