0d541334afcc5c1c2d8c93af3a77ce498cc454cd
ysmike <xcalibur220@gmail.com>
2022-01-18 14:10:16 -0500

    initialize jest and write tests for the `formatMoney` func

--- frontend/jest.setup.js
+++ frontend/jest.setup.js
@@ -1,3 +1,5 @@
 import '@testing-library/jest-dom';
 
+// to suppress warning saying there is no access to `alert` because jest doesn't
+// use browsers thus it doesn't have access to the window object
 window.alert = console.log;
