a799238a02a7b30b43cb652aa94663a1db50ad9d
ysmike <xcalibur220@gmail.com>
2022-01-19 12:27:23 -0500

    comment out the account section from the nav
    restore this when account page is completed

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -15,7 +15,7 @@ export default function Nav() {
         <>
           <Link href="/sell">Sell</Link>
           <Link href="/orders">Orders</Link>
-          <Link href="/account">Account</Link>
+          {/* <Link href="/account">Account</Link> */}
           <SignOut />
           <button type="button" onClick={openCart}>
             My Cart
