c9001b2a612bf9db014f8d75e9601a033a291196
ysmike <xcalibur220@gmail.com>
2022-01-21 09:29:49 -0500

    add and configure favicon

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -16,7 +16,9 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en-US">
-        <Head />
+        <Head>
+          <link rel="shortcut icon" href="/static/favicon.ico" />
+        </Head>
         <body>
           <Main />
           <NextScript />
